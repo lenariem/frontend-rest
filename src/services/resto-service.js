4f734623ce3b2efa0f0a37a747757c0650d680c3
unknown <olena.denysova88@gmail.com>
2021-01-19 11:32:14 +0100

    pp ihn target blank

--- src/services/resto-service.js
+++ src/services/resto-service.js
@@ -2,8 +2,8 @@
 
 export default class RestoService{
     _apiBase = 'http://localhost:5000'
-    /* _apiBase = 'https://backend-rest-react.herokuapp.com' */
-
+    /* _apiBase = 'https://backend-rest-react.herokuapp.com'
+ */
     async getResource(url) {
         const urlToSee = `${this._apiBase}${url}`
         console.log("url:" + urlToSee)
